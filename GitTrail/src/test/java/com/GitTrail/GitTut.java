ddb39c98f56948c8ecc4e6d303c9050a386119da
Keerthireddyjakkam <keerthireddyjakkam@gmail.com>
2020-08-24 23:42:37 +0530

    first commit

---
+++ GitTrail/src/test/java/com/GitTrail/GitTut.java
@@ -0,0 +1,19 @@
+package com.GitTrail;
+
+import static org.junit.Assert.assertTrue;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class GitTut {
+
+	@Test
+	
+	public void login() {
+  Assert.assertEquals(5, 5);
+		
+	}
+	
+	
+}
